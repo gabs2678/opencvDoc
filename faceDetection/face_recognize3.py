8d251f8f2d361a5261d7edc93420454c2f7530d3
Gabriel <gabriel@Gabriels-MacBook-Air-2.local>
2024-06-07 11:32:00 -0400

    decreasing tolerance

--- faceDetection/face_recognize3.py
+++ faceDetection/face_recognize3.py
@@ -33,7 +33,7 @@ while True:
         face_names = []
         for face_encoding in face_encodings:
             # Check if the face matches the reference face
-            matches = face_recognition.compare_faces([reference_face_encoding], face_encoding)
+            matches = face_recognition.compare_faces([reference_face_encoding], face_encoding, tolerance=0.4)
             name = "Unknown"
 
             # Use the known face with the smallest distance to the new face
